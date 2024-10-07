d45360a202a1269647c73d9f3fc0a0066830e21a
Mauricio Arce <mauarce34@gmail.com>
2024-10-07 19:00:11 -0300

    Update envCon.ts

--- back/src/config/envCon.ts
+++ back/src/config/envCon.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+dotenv.config();
 
 const PORT: number | string = Number(process.env.PORT) || 3000;
 const DB_HOST = process.env.DB_HOST || "localhost";
